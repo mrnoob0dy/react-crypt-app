933db78135d03035024ce47ee62aa31477c65da9
mrnoob0dy <jayna5155@yandex.ru>
2025-04-09 16:37:38 +0300

    Second commit

---
+++ src/components/layout/AppSider.jsx
@@ -0,0 +1,13 @@
+import { Layout } from "antd";
+
+const siderStyle = {
+  padding: "1rem",
+};
+
+export function AppSider() {
+  return (
+    <Layout.Sider width="25%" style={siderStyle}>
+      SIder
+    </Layout.Sider>
+  );
+}
